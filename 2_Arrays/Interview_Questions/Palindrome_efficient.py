6929c9c0699d9c39ca5dcc040c94487e33f6152d
saigiridhar.varanasi <saigiridhar.varanasi@target.com>
2021-04-22 10:11:52 +0530

    Update Palindrome_efficient.py

--- 2_Arrays/Interview_Questions/Palindrome_efficient.py
+++ 2_Arrays/Interview_Questions/Palindrome_efficient.py
@@ -6,15 +6,16 @@ Created on Thu Apr 22 09:26:09 2021
 @author: z003z7n
 """
 
-given_string = 'radadar'
+given_string = 'raddar'
 
+#way 1
 def palindrome_determiner(string):
     
     start_index = 0
     end_index = len(string) - 1
     
     while end_index > start_index:
-        
+        print('comparing {} with {}'.format(string[end_index], string[start_index]))
         #comparing logic
         if(string[end_index] == string[start_index]):
             #increment start_index
@@ -29,6 +30,15 @@ def palindrome_determiner(string):
         print('Palindrome')
 
 palindrome_determiner(given_string)
+
+#way 2
+def palindrome_pythonic(string):
+    if string == string[::-1]:
+        return True
+    else:
+        return False
+print('Pythonic Way')
+print('Palindrome' if palindrome_pythonic(given_string) else 'Not Palindromme')
         
         
         
